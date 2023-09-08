2a9643b484f639b580bd8cf5190e7d469a470f3b
Regulus0811 <kbc6268@g.yju.ac.kr>
2023-09-08 20:17:48 +0900

    movie app part two

--- src/App.js
+++ src/App.js
@@ -1,28 +1,7 @@
-import { useState, useEffect } from "react";
-
-function Hello() {
-  useEffect(() => {
-    console.log("Hi :) ");
-    return () => console.log("bye :(");
-  }, []);
-  // useEffect(function () {
-  //   console.log("Hi :) ");
-  //   return function () {
-  //     console.log("bye :(");
-  //   };
-  // }, []);
-  return <h1>Hello!</h1>;
-}
+import Home from "./routes/Home";
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
-  return (
-    <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
---
+++ src/components/Movie.jsx
@@ -0,0 +1,25 @@
+import PropTypes from "prop-types";
+
+const movie = ({ coverImg, title, summary, genres }) => {
+  return (
+    <div>
+      <img src={coverImg} alt={title} />
+      <h2>{title}</h2>
+      <p>{summary}</p>
+      <ul>
+        {genres.map((genre) => (
+          <li key={genre}>{genre}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+movie.propTypes = {
+  coverImg: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  genre: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default movie;
